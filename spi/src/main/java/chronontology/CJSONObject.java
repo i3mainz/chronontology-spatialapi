ef6d4d7c6da90cce767e88b279999bf694d8ee87
Florian Thiery <web@florian-thiery.de>
2017-08-03 10:21:07 +0200

    add class for CJSONObject

---
+++ spi/src/main/java/chronontology/CJSONObject.java
@@ -0,0 +1,13 @@
+package chronontology;
+
+import org.json.simple.JSONObject;
+
+public class CJSONObject {
+    
+    private static JSONObject cjson;
+    
+    public CJSONObject() {
+        cjson = new JSONObject();
+    }
+    
+}
