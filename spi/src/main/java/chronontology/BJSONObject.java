7c8b404d9b93e4ee9876c89582e8d3d31143ed3e
Florian Thiery <web@florian-thiery.de>
2017-08-03 10:23:09 +0200

    add class for BoundBox JSONObject

---
+++ spi/src/main/java/chronontology/BJSONObject.java
@@ -0,0 +1,13 @@
+package chronontology;
+
+import org.json.simple.JSONObject;
+
+public class BJSONObject {
+    
+    private static JSONObject bjson;
+    
+    public BJSONObject() {
+        bjson = new JSONObject();
+    }
+    
+}
