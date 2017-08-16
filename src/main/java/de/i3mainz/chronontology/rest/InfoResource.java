4bd17e1cfcfa591ce1d5457e55a0d6c0e8889c1c
Florian Thiery <web@florian-thiery.de>
2017-08-16 15:33:06 +0200

    add info to REST root

---
+++ src/main/java/de/i3mainz/chronontology/rest/InfoResource.java
@@ -0,0 +1,27 @@
+package de.i3mainz.chronontology.rest;
+
+import de.i3mainz.chronontology.errorlog.Logging;
+import de.i3mainz.chronontology.pom.POM;
+import de.i3mainz.chronontology.restconfig.ResponseGZIP;
+import java.io.IOException;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class InfoResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
+    public Response getInfo() {
+        try {
+            return ResponseGZIP.setResponse("gzip", POM.getInfo().toJSONString());
+        } catch (IOException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(Logging.getMessageJSON(e, "de.i3mainz.chronontology.rest.InfoResource"))
+                    .header("Content-Type", "application/json;charset=UTF-8").build();
+        }
+    }
+
+}
