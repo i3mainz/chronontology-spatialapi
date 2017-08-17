330a133d79ab723045ad90310278a65d02fecced
Florian Thiery <web@florian-thiery.de>
2017-08-17 08:11:12 +0200

    set CJSONObject as child from JSONObject

--- src/main/java/de/i3mainz/chronontology/chronontology/CJSONObject.java
+++ src/main/java/de/i3mainz/chronontology/chronontology/CJSONObject.java
@@ -1,17 +1,49 @@
 package de.i3mainz.chronontology.chronontology;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 /**
  * JSONObject to represent spatial data of a chronontology period
  * @author Florian Thiery
  */
-public class CJSONObject {
+public class CJSONObject extends JSONObject {
     
-    private static JSONObject cjson;
+    private JSONObject geojson = new JSONObject();
+    private String type = "";
+    private JSONArray features = new JSONArray();
+    private JSONObject metadata = new JSONObject();
     
     public CJSONObject() {
-        cjson = new JSONObject();
+        super();
+        geojson = new JSONObject();
+        geojson.put("type", type);
+        geojson.put("features", features);
+        geojson.put("metadata", metadata);
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public JSONArray getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(JSONArray features) {
+        this.features = features;
+    }
+
+    public JSONObject getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(JSONObject metadata) {
+        this.metadata = metadata;
     }
     
 }
