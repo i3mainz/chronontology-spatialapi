3ebf18a5e279c58aac7911e48214c482f767cbce
Florian Thiery <web@florian-thiery.de>
2017-08-17 14:59:16 +0200

    rename to GGeoJSONObject

---
+++ src/main/java/de/i3mainz/chronontology/gazetteer/GGeoJSONObject.java
@@ -0,0 +1,49 @@
+package de.i3mainz.chronontology.gazetteer;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+/**
+ * JSONObject to store gazetter search results
+ * @author Florian Thiery
+ */
+public class GGeoJSONObject extends JSONObject {
+    
+    private JSONObject geojson = new JSONObject();
+    private String type = "";
+    private JSONArray features = new JSONArray();
+    private JSONObject metadata = new JSONObject();
+    
+    public GGeoJSONObject() {
+        super();
+        geojson = new JSONObject();
+        geojson.put("type", type);
+        geojson.put("features", features);
+        geojson.put("metadata", metadata);
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public JSONArray getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(JSONArray features) {
+        this.features = features;
+    }
+
+    public JSONObject getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(JSONObject metadata) {
+        this.metadata = metadata;
+    }
+    
+}
