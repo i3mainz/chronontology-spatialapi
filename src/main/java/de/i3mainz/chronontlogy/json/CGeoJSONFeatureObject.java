a9f751e5693380ca68d26e50d7655c65b41cf7c6
Florian Thiery <web@florian-thiery.de>
2017-08-18 09:43:30 +0200

    add new CGeoJSONFeatureObject

---
+++ src/main/java/de/i3mainz/chronontlogy/json/CGeoJSONFeatureObject.java
@@ -0,0 +1,36 @@
+package de.i3mainz.chronontlogy.json;
+
+import org.json.simple.JSONObject;
+
+/**
+ * JSONObject to store single feature information for CGeoJON objects
+ *
+ * @author Florian Thiery
+ */
+public class CGeoJSONFeatureObject extends GGeoJSONObject {
+
+    public CGeoJSONFeatureObject() {
+        super();
+    }
+
+    /**
+     * set GeoJSON single feature properties
+     *
+     * @param url
+     * @param gazetteerid
+     * @param type
+     * @param names
+     * @param relation
+     */
+    public void setProperties(String url, String gazetteerid, String type, NamesJSONObject names, String relation) {
+        JSONObject properties = new JSONObject();
+        super.remove("properties");
+        properties.put("@id", url);
+        properties.put("gazetteerid", gazetteerid);
+        properties.put("type", type);
+        properties.put("names", names);
+        properties.put("relation", relation);
+        super.put("properties", properties);
+    }
+
+}
