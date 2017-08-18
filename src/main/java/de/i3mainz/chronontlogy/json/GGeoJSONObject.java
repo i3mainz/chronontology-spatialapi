a8be32f9247b719631542a7571909118995280b4
Florian Thiery <web@florian-thiery.de>
2017-08-18 14:33:15 +0200

    add first try for geojson-ld in /spi/geojson/g?geojsonld=true

--- src/main/java/de/i3mainz/chronontlogy/json/GGeoJSONObject.java
+++ src/main/java/de/i3mainz/chronontlogy/json/GGeoJSONObject.java
@@ -1,6 +1,12 @@
 package de.i3mainz.chronontlogy.json;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 /**
  * JSONObject to store gazetter search results
@@ -18,15 +24,17 @@ public class GGeoJSONObject extends JSONObject {
 
     /**
      * set GeoJSON geometry
-     * @param geometry 
+     *
+     * @param geometry
      */
     public void setGeometry(JSONObject geometry) {
         super.remove("geometry");
         super.put("geometry", geometry);
     }
-    
+
     /**
      * get geometry
+     *
      * @return geometry json object
      */
     public JSONObject getGeometry() {
@@ -35,10 +43,11 @@ public class GGeoJSONObject extends JSONObject {
 
     /**
      * set GeoJSON properties
+     *
      * @param url
      * @param gazetteerid
      * @param type
-     * @param names 
+     * @param names
      */
     public void setProperties(String url, String gazetteerid, String type, NamesJSONObject names) {
         JSONObject properties = new JSONObject();
@@ -49,13 +58,65 @@ public class GGeoJSONObject extends JSONObject {
         properties.put("names", names);
         super.put("properties", properties);
     }
-    
+
     /**
      * get properties
+     *
      * @return properties json object
      */
     public JSONObject getProperties() {
         return (JSONObject) super.get("properties");
+    }
+
+    public JSONObject getJSONLD() throws IOException {
+        try {
+            JSONObject jsonld = this;
+            JSONObject contexts = new JSONObject();
+            // read GeoJSON-LD Context
+            JSONObject data = new JSONObject();
+            URL obj = new URL("https://raw.githubusercontent.com/geojson/geojson-ld/gh-pages/geojson-context.jsonld");
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            if (con.getResponseCode() == 200) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                data = (JSONObject) new JSONParser().parse(response.toString());
+            }
+            JSONObject context = (JSONObject) data.get("@context");
+            contexts.putAll(context);
+            jsonld.put("@context", context);
+            // read GeoJSON-LD-LG Context
+            obj = new URL("https://raw.githubusercontent.com/florianthiery/geojson-ld-lg/master/geojson-context-lg.jsonld");
+            con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            if (con.getResponseCode() == 200) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                data = (JSONObject) new JSONParser().parse(response.toString());
+            }
+            context = (JSONObject) data.get("@context");
+            contexts.putAll(context);
+            jsonld.put("@context", contexts);
+            // delete big properties for testing
+            jsonld.remove("geometry");
+            jsonld.remove("properties");
+            // set test properties from geojson-lg
+            jsonld.put("relation", "test");
+            jsonld.put("metadata", new JSONObject());
+            return jsonld;
+        } catch (Exception e) {
+            return null;
+        }
     }
 
 }
